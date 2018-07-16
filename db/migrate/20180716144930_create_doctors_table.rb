151957da1a5872a000fb994805db51e60c9df785
Matt Farmer <mrfarmer777@gmail.com>
2018-07-16 14:49:46 +0000

    automatically backed up by learn

--- db/migrate/20180716144930_create_doctors_table.rb
+++ db/migrate/20180716144930_create_doctors_table.rb
@@ -1,6 +1,6 @@
 class CreateDoctorsTable < ActiveRecord::Migration
   def change
-    create_table :doctors_tables do |t|
+    create_table :doctors do |t|
       t.string :name
       t.string :department
     end
